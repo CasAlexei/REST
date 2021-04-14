117ca8031bb784031833778cb468f0a2852c98f3
CasAlexei <hp81@mail.ru>
2021-04-14 15:09:14 +0300

    Initial commit

---
+++ src/main/java/com/endava/internship/dataCenter/DataCenterApplication.java
@@ -0,0 +1,13 @@
+package com.endava.internship.dataCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endava/internship/dataCenter/DataCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.internship.dataCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
