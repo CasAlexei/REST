983e058aba511210c79e51f53255cf46c40b9662
CasAlexei <hp81@mail.ru>
2021-04-16 12:25:05 +0300

    make two tables to check relationship OneToOne

--- src/main/resources/db/migration/V20191004032100__create_table_jobs.sql
+++ src/main/resources/db/migration/V20191004032100__create_table_jobs.sql
@@ -4,12 +4,14 @@ CREATE TABLE jobs
     job_title  VARCHAR2(35) NOT NULL,
     min_salary NUMBER(6),
     max_salary NUMBER(6),
+
     CONSTRAINT min_max_salary_ck CHECK ( min_salary < max_salary )
 );
-
-COMMENT ON TABLE jobs IS 'jobs table with job titles and salary ranges. Contains 19 rows.
-References with employees and job_history table.';
-COMMENT ON COLUMN jobs.job_id IS 'Primary key of jobs table.';
-COMMENT ON COLUMN jobs.job_title IS 'A not null column that shows job title, e.g. AD_VP, FI_ACCOUNTANT';
-COMMENT ON COLUMN jobs.min_salary IS 'Minimum salary for a job title.';
-COMMENT ON COLUMN jobs.max_salary IS 'Maximum salary for a job title';
+--
+--
+--CREATE SEQUENCE jobs_seq NOCACHE;
+--
+--
+--ALTER TABLE jobs
+--    MODIFY job_id DEFAULT jobs_seq.nextval;  --whe use sequence to generate id. nextval give a next id by id++
+--
--- src/main/resources/db/migration/V20191004032600__create_table_regions.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE regions
-(
-    region_id   NUMBER PRIMARY KEY,
-    region_name VARCHAR2(25)
-);
